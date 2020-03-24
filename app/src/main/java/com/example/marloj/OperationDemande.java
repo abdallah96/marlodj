aa54fd26396f2b3de48231a01aee9800c5da588c
Abdallah <abdalah@whatever.com>
2020-03-24 22:37:34 +0300

    Project design

---
+++ app/src/main/java/com/example/marloj/OperationDemande.java
@@ -0,0 +1,13 @@
+package com.example.marloj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OperationDemande extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operation_demande);
+    }
+}
